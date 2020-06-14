80d209128818d56428350c402e7f86c359569520
Learn SAPCFPortal <learnscpportalcf@gmail.com>
2020-06-14 19:16:46 +0000

    add pipeline

---
+++ .github/actions/deploy_mta/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -l
+cd ${GITHUB_WORKSPACE}
+echo 'Connect to CF'
+cf login -a ${INPUT_HOST} -o 
+${INPUT_ORG} -u ${INPUT_USER} -s ${INPUT_SPACE} -p ${INPUT_PASSWORD}
+echo 'Running the mtabuilder...'
+npm config set @sap:registry https://npm.sap.com
+mbt build -p=cf -t=../ --mtar=myportal.mtar 
+echo 'Deploy'
+cf deploy ../myportal.mtar -f 
