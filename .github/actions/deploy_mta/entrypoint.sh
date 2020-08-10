1189f9cea81c5f97ca6071fc35ba058414a62ea7
steveguo1984 <steve.guo@outlook.com>
2020-08-10 23:42:52 +0200

    Update entrypoint.sh

--- .github/actions/deploy_mta/entrypoint.sh
+++ .github/actions/deploy_mta/entrypoint.sh
@@ -4,7 +4,9 @@ echo 'Connect to CF'
 cf login -a ${INPUT_HOST} -o ${INPUT_ORG} -u ${INPUT_USER} -s ${INPUT_SPACE} -p ${INPUT_PASSWORD}
 echo 'Running the mtabuilder...'
 npm config set @sap:registry https://npm.sap.com
+mbt build -p=cf -t=../ --mtar=myportal.mtar
 mbt build -p=cf -t=../ --mtar=myportal.mtar 
+
 
 echo 'Deploy'
 cf install-plugin multiapps -f
